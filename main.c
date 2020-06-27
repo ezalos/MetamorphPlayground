3f3000f51271f280c44d61cef0e2e1c3d3e11aed
ezalos <ezalos@gmail.com>
2020-06-27 18:03:07 +0200

    Working for less than hello world (return int and int*)

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stddef.h>    // size_t
+#include <stdint.h>    // uint8_t, uint32_t
+#include <stdio.h>     // printf
+#include <string.h>    // memcpy
+#include <sys/mman.h>  // mmap, mprotect, munmap, MAP_FAILURE
+#include <unistd.h>
+
+// Machine code for "mov eax, 12345678h" followed by "ret"
+//uint8_t machine_code[] = { 0xB8, 0x78, 0x56, 0x34, 0x12, 0xC3 };
+
+uint32_t	new(uint32_t *size)
+{
+	return(*size);
+}
+
+int main(int argc, char **argv) {
+    // Allocate a new page of memory, setting its protections to read+write
+	size_t fct_size = (size_t)&main - (size_t)&new;
+//	fct_size = sizeof(machine_code);
+
+    void *mem = mmap(NULL, fct_size, PROT_READ | PROT_WRITE,
+        MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    if (mem == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    // Write the machine code into the newly allocated page
+    memcpy(mem, &new, fct_size);
+
+    // Change the page protections to read+execute
+    if (mprotect(mem, fct_size, PROT_READ | PROT_EXEC) == -1) {
+        perror("mprotect");
+        return 2;
+    }
+    // Point a function pointer at the newly allocated page, then call it
+    uint32_t(*fn)(uint32_t*) = (uint32_t(*)(uint32_t*)) mem;
+    uint32_t tes = 42;
+    uint32_t result = fn(&tes);
+    printf("result = %d\n", result);
+
+    // Free the memory
+    if (munmap(mem, fct_size) == -1) {
+        perror("munmap");
+        return 3;
+    }
+
+    return 0;
+}
